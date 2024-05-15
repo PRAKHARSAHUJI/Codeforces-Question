59991153b9aca1c3499f2446565b623d9c88468f
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-15 12:51:39 +0530

    Hulk 800 Rating

--- 800 RATING/Hulk.cpp
+++ 800 RATING/Hulk.cpp
@@ -1,27 +1,5 @@
 // https://codeforces.com/contest/705/problem/A
 
-// #include <bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-// int n;
-// cin>>n;
-
-// for(int i =1 ; i < n ; i=i+1)
-// {
-// if(i%2==0)
-// cout<<"I love that ";
-// else
-// cout<<"I hate that ";
-
-// }
-// if(n%2==0)
-// cout<<"I love it";
-// else
-// cout<<"I hate it";
-
-// }
-
 // ------------------------------------ Correct Code  ---------------------------------------------
 
 #include <bits/stdc++.h>
@@ -30,24 +8,46 @@ int main()
 {
     int n;
     cin >> n;
-    if (n == 1)
-        cout << "I hate it";
-    else
-    {
-        for (int i = 1; i < n; i = i + 1)
-        {
-            if (i % 2 == 0)
-                cout << "I love that ";
-            else
-                cout << "I hate that ";
-        }
 
-        if (n % 2 == 0)
-            cout << "I love it";
+    for (int i = 1; i < n; i = i + 1)
+    {
+        if (i % 2 == 0)
+            cout << "I love that ";
         else
-            cout << "I hate it";
+            cout << "I hate that ";
     }
+    if (n % 2 == 0)
+        cout << "I love it";
+    else
+        cout << "I hate it";
 }
+
+// ------------------------------------ Correct Code  ---------------------------------------------
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     if (n == 1)
+//         cout << "I hate it";
+//     else
+//     {
+//         for (int i = 1; i < n; i = i + 1)
+//         {
+//             if (i % 2 == 0)
+//                 cout << "I love that ";
+//             else
+//                 cout << "I hate that ";
+//         }
+
+//         if (n % 2 == 0)
+//             cout << "I love it";
+//         else
+//             cout << "I hate it";
+//     }
+// }
 
 // ------------------------------------ INCorrect Code  ---------------------------------------------
 
