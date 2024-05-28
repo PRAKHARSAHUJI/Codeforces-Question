5505021bd1aeb7eedf76d7cf28e01a26bad3a2e1
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-28 14:41:43 +0530

    Greedy,Math

--- TLE Eliminators CP - 31  ( 800 Ratings )/02_Line_trip.cpp
+++ TLE Eliminators CP - 31  ( 800 Ratings )/02_Line_trip.cpp
@@ -7,32 +7,26 @@ int main()
 {
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
-        int n,x;
-        cin>>n>>x;
-        int min_val = 0; 
-        vector<int> v(n+2);
-        v[0]=0;
-        for(int i = 1 ; i <= n ; i++ )
+        int n, x;
+        cin >> n >> x;
+        int min_val = 0;
+        vector<int> v(n + 2);
+        v[0] = 0;
+        for (int i = 1; i <= n; i++)
         {
-            cin>>v[i];
+            cin >> v[i];
         }
-        v[n+1]=x;
-        for(int i = 1 ; i <= n+1 ; i++ )
+        v[n + 1] = x;
+        for (int i = 1; i <= n + 1; i++)
         {
-           int mx = v[i]-v[i-1];
-           if(i==(n+1))
-           mx*=2;
-           if(mx>min_val)
-           min_val=mx;
-
+            int mx = v[i] - v[i - 1];
+            if (i == (n + 1))
+                mx *= 2;
+            if (mx > min_val)
+                min_val = mx;
         }
-        cout<<min_val<<"\n";
-
+        cout << min_val << "\n";
     }
 }
-
-
-
-
