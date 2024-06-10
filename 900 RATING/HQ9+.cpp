5c43d4363add3149dc99a15fa4e32cc8207ee0b1
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-06-11 00:32:59 +0530

    Implementation 900 RATING

---
+++ 900 RATING/HQ9+.cpp
@@ -0,0 +1,54 @@
+// https://codeforces.com/problemset/problem/133/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    bool flag = false ;
+    for (int i =0 ; i < s.length();i++)
+    {
+        if(s[i]=='H'|| s[i]=='Q' ||  s[i]=='9')
+        {
+        cout<<"YES";
+        return 0;
+        }
+        else
+        {
+        flag = true;
+        }
+    }
+    if(flag)
+    cout<<"NO";
+
+    return 0;
+}
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string s;
+//     cin>>s;
+
+//     for (int i =0 ; i < s.length();i++)
+//     {
+//         if(s[i]=='H'|| s[i]=='Q' ||  s[i]=='9' || s[i]=='+' )
+//         {
+//         cout<<"YES";
+//         return 0;
+//         }
+//         else
+//         {
+//         cout<<"NO";
+//         return 0;
+//         }
+
+//     }
+//     return 0;
+// }
+
