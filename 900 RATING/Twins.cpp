47f5a65c26d1fd68cb9106d5f69843476b7ca1b3
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-06-15 17:19:02 +0530

    Greedy Sortings

---
+++ 900 RATING/Twins.cpp
@@ -0,0 +1,64 @@
+// https://codeforces.com/problemset/problem/160/A
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//         sum += arr[i];
+//     }
+
+//     sort(arr, arr + n, greater<int>());
+    
+//     int coins = 0;
+//     int sum2 = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum2 += arr[i];
+//         coins++;
+//         if (sum2 > sum - sum2)
+//         {
+//             cout << coins;
+//             break;
+//         }
+//     }
+// }
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr, arr + n, greater<int>());
+    
+    int sum2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum2 += arr[i];
+        if (sum2 > sum - sum2)
+        {
+            cout << i+1;
+            break;
+        }
+    }
+}
