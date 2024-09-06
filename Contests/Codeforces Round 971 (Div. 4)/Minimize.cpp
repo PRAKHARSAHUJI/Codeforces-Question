c5e5116cc67ba9971b0fe71030dddc69519d8c3d
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-09-06 10:57:59 +0530

    Brute Force , Math

---
+++ Contests/Codeforces Round 971 (Div. 4)/Minimize.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/contest/2009/problem/A
+
+// Brute Force , Math
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<b-a;
+    }
+    
+}
