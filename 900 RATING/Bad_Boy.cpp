ed22a1999c8d5fb75a9fbe0e8e879b641a392ff4
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2025-01-10 19:31:05 +0530

    Constructive Algorithms Greedy Math *900 Rating

---
+++ 900 RATING/Bad_Boy.cpp
@@ -0,0 +1,21 @@
+
+// https://codeforces.com/problemset/problem/1537/B
+
+// -------------------------------------Correct Code---------------------------------------------
+
+// Constructive Algorithms Greedy Math *900 Rating
+
+#include "bits/stdc++.h"
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int length, breadth, i, j;
+        cin >> length >> breadth >> i >> j;
+ 
+        cout << 1 << " " << 1 << " "; // put ball in top right
+        cout << length << " " << breadth << "\n"; // put ball in bottom left
+    }
+}
