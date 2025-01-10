b601afa3bd0db9b52abbfdbdc18f6aa65e06ebc9
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2025-01-10 20:18:32 +0530

    Brute Force DP Implementation  Math *900 Rating

---
+++ 900 RATING/Filling_Diamonds.cpp
@@ -0,0 +1,27 @@
+
+// https://codeforces.com/problemset/problem/1339/A
+
+// Brute Force DP Implementation Math *900 Rating
+
+//  ------------------------------------------------- Correct Code  ----------------------------------------------
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << n << endl;
+	}
+}
+
+// 1 - 2
+// 2 - 6
+// 3 - 10
+// 4 - 14
